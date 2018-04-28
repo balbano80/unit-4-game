8d430160cbd80e526485296e44d218d12ba77ad0
Bryan Albano <albanobryan@yahoo.com>
2018-04-27 22:23:39 -0700

    somewhat rudimentary way of creating buttons with value names same as object attibutes and set to each characters values.  Also played around with images to get properly aligned images as backgrounds for each button.  Currently able to have them display at the new game/default postion on page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,128 @@
 
 
+// var characterArr =[  
+    var sarah ={ 
+        hp: 100,
+        ap: 10,
+        cap: 20,
+        image: "assets/images/sarah.jpg"
+    }
 
-var sarah = {
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 20
-};
+    var jareth = {
+        hp: 150,
+        ap: 15,
+        cap: 25,
+        image: "../images/jareth.jpg"
+    }
 
-var jareth = {
-    healthPoints: 150,
-    attackPower: 15,
-    counterAttackPower: 25
-};
+    var hoggle = {
+        hp: 90,
+        ap: 11,
+        cap: 21,
+        image: "../images/hoggle.jpg"
+    }
 
-var hoggle = {
-    healthPoints: 90,
-    attackPower: 11,
-    counterAttackPower: 21
-};
+    var bluto = {
+        hp: 140,
+        ap: 14,
+        cap: 23,
+        image: "../images/bluto.jpg"
+    }
 
-var bluto = {
-    healthPoints: 140,
-    attackPower: 14,
-    counterAttackPower: 23
-};
+    var fireGang = {
+        hp: 120,
+        ap: 12,
+        cap: 22,
+        image: "../images/fireGang.jpg"
+    }
 
-var  fireGang= {
-    healthPoints: 120,
-    attackPower: 12,
-    counterAttackPower: 22
-};
+// ];
+var user;
+var enemy;
+
+$(document).ready(function() {
+
+    var sarahBtn = $("<button>");
+    sarahBtn.addClass("characterBtn");
+    sarahBtn.addClass("sarahBg");
+    sarahBtn.attr("hp", sarah.hp);
+    sarahBtn.attr("ap", sarah.ap);
+    sarahBtn.attr("cap", sarah.cap);
+    $("#characters").append(sarahBtn);
+
+    var jarethBtn = $("<button>");
+    jarethBtn.addClass("characterBtn");
+    jarethBtn.addClass("jarethBg");
+    jarethBtn.attr("hp", jareth.hp);
+    jarethBtn.attr("ap", jareth.ap);
+    sarahBtn.attr("cap", jareth.cap);
+    $("#characters").append(jarethBtn);
+
+    var hoggleBtn = $("<button>");
+    hoggleBtn.addClass("characterBtn");
+    hoggleBtn.addClass("hoggleBg");
+    hoggleBtn.attr("hp", hoggle.hp);
+    hoggleBtn.attr("ap", hoggle.ap);
+    hoggleBtn.attr("cap", hoggle.cap);
+    $("#characters").append(hoggleBtn);
+
+    var jarethBtn = $("<button>");
+    jarethBtn.addClass("characterBtn");
+    jarethBtn.addClass("blutoBg");
+    jarethBtn.attr("hp", jareth.hp);
+    jarethBtn.attr("ap", jareth.ap);
+    sarahBtn.attr("cap", jareth.cap);
+    $("#characters").append(jarethBtn);
+
+    var jarethBtn = $("<button>");
+    jarethBtn.addClass("characterBtn");
+    jarethBtn.addClass("fireGangBg");
+    jarethBtn.attr("hp", jareth.hp);
+    jarethBtn.attr("ap", jareth.ap);
+    sarahBtn.attr("cap", jareth.cap);
+    $("#characters").append(jarethBtn);
+
+    function reset(){
+        for(var i =0; i < characterArr.length; i++){
+            var characterButton = $("<button>");
+            characterButton.addClass("characterBtn");
+            characterButton.text(this.healthPoints);
+            characterButton.html(this.image);
+            $("#characters").append(characterButton);
+            console.log(characterButton);
+
+        }
+
+
+        $("#yourCharacter, #enemies, #opponent, #message").empty();       
+    
+    }
+
+    function characterSelect(){
+
+    } // will make clicked character to be user character and move it to your character div
+    
+    function enemySelect(){
+    
+    
+    } // will make clicked character to be user enemy and move it to the enemy div
+    
+    function attack(){
+    
+    } // will: 1. decrease opponents hit points
+            // 2. decrease users healthPoints by opponents counterAttackPower
+            // 3. update users attackPower by base power
+    
+    function checkHealth(){
+    
+    }// will 
+
+
+
+    $("").on("click", function() {
+
+
+    })
+
+    // reset();
+});
