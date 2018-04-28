cf5fdb0177a5dcfc7b476fac49aa33c15ccd61d7
Bryan Albano <albanobryan@yahoo.com>
2018-04-27 17:59:06 -0700

    additional setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+
+
+
+var sarah = {
+    healthPoints: 100,
+    attackPower: 10,
+    counterAttackPower: 20
+};
+
+var jareth = {
+    healthPoints: 150,
+    attackPower: 15,
+    counterAttackPower: 25
+};
+
+var hoggle = {
+    healthPoints: 90,
+    attackPower: 11,
+    counterAttackPower: 21
+};
+
+var bluto = {
+    healthPoints: 140,
+    attackPower: 14,
+    counterAttackPower: 23
+};
+
+var  fireGang= {
+    healthPoints: 120,
+    attackPower: 12,
+    counterAttackPower: 22
+};
